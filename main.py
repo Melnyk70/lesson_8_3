d2f86c8f9ebbc2d1d7b46d011c88213e55b0f582
MelnykIhor <melnyk70@gmail.com>
2024-10-19 12:20:20 +0300

    DZ_8_3

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ДЗ 8.3. Унікальне число
+# Вам необхідно написати функцію find_unique_value, яка приймає список із чисел,
+# знаходить серед них унікальне число та повертати його. Унікальне число - це число,
+# яке зустрічається в списку один раз. Випадок, коли в одному списку буде кілька унікальних чисел,
+# перевіряти не потрібно.
+# Приклад:
+# def find_unique_value(some_list):
+#    pass
+# assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+# assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+# assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+# print("ОК")
